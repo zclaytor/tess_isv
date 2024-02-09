9809060c5109f674a12e29035a8717251241432e
zclaytor <zclaytor@hawaii.edu>
2024-02-09 17:02:21 -0500

    copy-paste working code into main

--- eclipsing_binary_tests/measure_transits.py
+++ eclipsing_binary_tests/measure_transits.py
@@ -21,5 +21,53 @@ def load_ebs():
     return cvzs
 
 
+
 if __name__ == "__main__":
-    pass
+    cvzs = load_ebs()
+
+    plt.close("all")
+    for tic, row in cvzs.iterrows():
+        p0 = row["period"]
+        dur = row["prim_width_2g"]
+        
+        sr = lk.search_lightcurve(f"TIC {tic}", author="spoc", cadence=120)
+        lcs = sr.download_all(flux_column="sap_flux", quality_bitmask="hard")
+
+        if len(lcs) == 0:
+            continue
+            
+        sr = lk.search_targetpixelfile(f"TIC {tic}", author="spoc", cadence=120,
+            sector=[l.sector for l in lcs])
+        tpfs = sr.download_all(quality_bitmask="hard")
+        
+        path = f"ebs_astropy/{tic:010d}"
+        os.makedirs(path, exist_ok=True)
+        lcs = lcs[np.argsort([l.sector for l in lcs])]
+        
+        for lc in lcs:
+            sector = lc.sector
+            camera = lc.camera
+            ccd = lc.ccd
+            
+            print(tic, sector)
+
+            try:
+                pg = lc.to_periodogram("bls", minimum_period=p0/1.5, maximum_period=3*p0)
+            except ValueError:
+                print(f"Period ({p0:.3f}) and Duration ({dur:.3f}) break the periodogram. Skipping.")
+                continue
+                
+            ax = lc.plot()
+            ax.legend().remove()
+            
+            ymin, ymax = ax.get_ylim()
+            
+            med_flux = np.median(lc.flux.value)
+            
+            stats = pg.compute_stats()
+            ax.vlines(stats["transit_times"].value, ymin=ymin, ymax=ymax, color="k", linestyle=":", alpha=0.3)
+            ax.axhline(med_flux - stats["depth"][0].value, color="k")
+            ax.axhline(med_flux, color="k")
+            plt.tight_layout()
+            plt.savefig(os.path.join(path, f"TIC{tic:010d}-s{sector:04d}.png"))
+            plt.close()
