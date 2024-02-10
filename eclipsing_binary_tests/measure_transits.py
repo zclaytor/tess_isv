951d2a834e6e4bcd8e85fe11650eac60404848b8
zclaytor <zclaytor@hawaii.edu>
2024-02-10 01:12:48 -0500

    only download TPF; print transit info

--- eclipsing_binary_tests/measure_transits.py
+++ eclipsing_binary_tests/measure_transits.py
@@ -5,10 +5,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 import lightkurve as lk
+from lightkurve.interact import _get_corrected_coordinate as getcoord
 
 
 root = "ebs_astropy"
-
+download_dir="."
    
 def count_sectors(val):
     return len(val.split(","))
@@ -22,6 +23,12 @@ def load_ebs():
     ebs["n_sectors"] = ebs.sectors.apply(count_sectors)
     cvzs = ebs.query("n_sectors > 10").sort_index()
     return cvzs
+
+def get_pixels(tpf):
+    target_ra, target_dec, pm_corrected = getcoord(tpf)
+    pix_x, pix_y = tpf.wcs.all_world2pix([(target_ra, target_dec)], 0)[0]
+    target_x, target_y = tpf.column + pix_x, tpf.row + pix_y
+    return target_x, target_y
 
 def plot_transits(lc, stats, save_path):
     ax = lc.plot()
@@ -46,31 +53,50 @@ if __name__ == "__main__":
         p0 = row["period"]
         dur = row["prim_width_2g"]
         
-        sr = lk.search_lightcurve(f"TIC {tic}", author="spoc", cadence=120)
-        lcs = sr.download_all(flux_column="sap_flux", quality_bitmask="hard")
-
-        if len(lcs) == 0:
-            continue
-            
-        sr = lk.search_targetpixelfile(f"TIC {tic}", author="spoc", cadence=120,
-            sector=[l.sector for l in lcs])
-        tpfs = sr.download_all(quality_bitmask="hard")
+        sr = lk.search_targetpixelfile(f"TIC {tic}", author="spoc", cadence=120)
+        tpfs = sr.download_all(quality_bitmask="hardest", download_dir=download_dir)
+        if len(tpfs) == 0:
+            continue        
+        tpfs = tpfs[np.argsort([t.sector for t in tpfs])]
         
         path = os.path.join(root, f"{tic:010d}")
         os.makedirs(path, exist_ok=True)
-        lcs = lcs[np.argsort([l.sector for l in lcs])]
-        
 
-        for lc in lcs:
-            sector = lc.sector
-            camera = lc.camera
-            ccd = lc.ccd
+        for tpf in tpfs:
+            sector = tpf.sector
+            camera = tpf.camera
+            ccd = tpf.ccd
             
+            target_x, target_y = get_pixels(tpf)
+            crowdsap = tpf.hdu[1].header["CROWDSAP"]
+            flfrcsap = tpf.hdu[1].header["FLFRCSAP"]
+
+            lc = tpf.to_lightcurve()
+            lc = lc[lc["quality"] == 0].remove_nans()
+
             try:
-                pg = lc.to_periodogram("bls", minimum_period=p0/1.5, maximum_period=3*p0)
+                pg = lc.to_periodogram("bls", minimum_period=p0/1.5, maximum_period=1.5*p0)
             except ValueError:
                 print(f"Period ({p0:.3f}) and Duration ({dur:.3f}) break the periodogram. Skipping.")
                 continue
             
-            stats = pg.compute_stats()
+            period = pg.period_at_max_power.value
+            duration = pg.duration_at_max_power.value
+            btjd0 = pg.transit_time_at_max_power.value
+
+            mask = pg.get_transit_mask(period, duration, btjd0)
+            flux = lc.flux.value[~mask]
+            flux_median = np.median(flux)
+            flux_std = np.std(flux, ddof=1)
+
+            stats = pg.compute_stats(period, duration, btjd0)
+            primary_depth, primary_std = stats["depth"]
+
             plot_transits(lc, stats, save_path=os.path.join(path, f"TIC{tic:010d}-s{sector:04d}.png"))
+            print(tic, sector, camera, ccd, 
+                target_x, target_y, crowdsap, flfrcsap,
+                period, duration, btjd0,
+                flux_median, flux_std,
+                primary_depth.value, primary_std.value, 
+                #secondary_depth, secondary_std, 
+                sep=",")
